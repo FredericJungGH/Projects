ad416e34324dc9633b838693d55c35a47cf017ed
Frederic Jung <101398371+FredericJungGH@users.noreply.github.com>
2022-06-30 09:09:11 +0200

    Update

--- DungeonWorld/JS/DungWorScripts.js
+++ DungeonWorld/JS/DungWorScripts.js
@@ -1,13 +1,21 @@
-function OpenTab(evt, cityName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " w3-red";
+document.getElementById("homeBtn").addEventListener("click", openPage('homeBtn'));
+document.getElementById("newsBtn").addEventListener("click", openPage('newsBtn'));
+document.getElementById("contactBtn").addEventListener("click", openPage('contactBtn'));
+document.getElementById("aboutBtn").addEventListener("click", openPage('aboutBtn'));
+
+function openPage(pageName) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+}
+
+    function doThis() {
+      var testElem = document.getElementById("2").value;
+      document.getElementById('3').value = testElem;
+    }
